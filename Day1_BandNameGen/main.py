437c6b93b154353519006a95f3d1c8d85e431869
Zachary Donnelly <zacharyodonnelly@protonmail.com>
2022-12-02 16:41:02 -0700

    Completed Day 1 of Udemy 100 Days of Python

---
+++ Day1_BandNameGen/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+users_city = input("What's name of the city you grew up in?\n")
+users_pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + users_city + " " + users_pet_name)
